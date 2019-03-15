a11dfeb4dafe4b55e25cf856d1a6e440e28f1864
Tjark Kühl <tjark-kuehl@hotmail.com>
2019-03-15 21:21:53 +0100

    Fonts & Scss defaults

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import createRouter from './router'
 import createStore from './store'
 
+import 'normalize.css'
+
 Vue.config.productionTip = false
 
 export default () => {
