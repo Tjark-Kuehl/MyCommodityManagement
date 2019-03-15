bab6df8807e6ea4247d77ac1e7b00d6b63d1c245
Tjark Kühl <tjark-kuehl@hotmail.com>
2019-03-15 21:11:19 +0100

    UVue SSR setup

---
+++ Frontend/uvue.config.js
@@ -0,0 +1,14 @@
+export default {
+    plugins: [
+        '@uvue/core/plugins/asyncData',
+        [
+            '@uvue/core/plugins/vuex',
+            {
+                onHttpRequest: true,
+                fetch: true
+            }
+        ],
+        '@uvue/core/plugins/middlewares',
+        '@uvue/core/plugins/errorHandler'
+    ]
+}
