5ea7f6225fadc6e4db37d87ab4380a3e886d8757
Tjark Kühl <tjark-kuehl@hotmail.com>
2019-03-23 21:49:45 +0100

    Added backend routing

--- Backend/php/index.php
+++ Backend/php/index.php
@@ -5,42 +5,49 @@ ob_start('ob_gzhandler');
 
 // no-cors fix
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Development");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 header_remove("X-Powered-By");
 
 include_once "./includes/inc.database.php";
 include_once "./classes/class.germanDate.php";
+include_once "./classes/class.request.php";
+include_once "./classes/class.router.php";
 
-/* Early catch api requests */
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    $response = new stdClass();
-    $error = null;
-    $data = json_decode(trim(file_get_contents("php://input")));
+$router = new Router(new Request);
+$router->get('/test', function () {
+    return "hello";
+});
 
-    switch ($data->action) {
-        case '':
-            if (!$data->xyz) {
-                $error = "Error xyz is not set!";
-                break;
-            }
-            break;
-        default:
-            $error = "Action is not set!";
-            break;
-    }
+// /* Early catch api requests */
+// if ($_SERVER['REQUEST_METHOD'] === "POST") {
+//     $response = new stdClass();
+//     $error = null;
+//     $data = json_decode(trim(file_get_contents("php://input")));
 
-    /* Check error response */
-    if (is_null($error)) {
-        $response->success = true;
-    } else {
-        $response->msg = $error;
-        $response->success = false;
-    }
+//     switch ($data->action) {
+//         case '':
+//             if (!$data->xyz) {
+//                 $error = "Error xyz is not set!";
+//                 break;
+//             }
+//             break;
+//         default:
+//             $error = "Action is not set!";
+//             break;
+//     }
 
-    echo json_encode($response);
-    die();
-}
+//     /* Check error response */
+//     if (is_null($error)) {
+//         $response->success = true;
+//     } else {
+//         $response->msg = $error;
+//         $response->success = false;
+//     }
+
+//     echo json_encode($response);
+//     die();
+// }
 
 ob_end_flush();
