749c5203eca3abd889d2b066d3ab19f20752e4c8
Tjark Kühl <tjark-kuehl@hotmail.com>
2019-03-29 10:55:18 +0100

    Responsive header

--- Frontend/src/components/TheHeader.vue
+++ Frontend/src/components/TheHeader.vue
@@ -1,17 +1,25 @@
 <template>
     <header>
+        <a class="menu">
+            <MenuIcon></MenuIcon>
+        </a>
         <RouterLink :to="{ name: 'home' }" tag="a">
             <Logo></Logo>
         </RouterLink>
+        <a class="menu hidden">
+            <MenuIcon></MenuIcon>
+        </a>
     </header>
 </template>
 
 <script>
 import Logo from '@/assets/icons/logo.svg'
+import MenuIcon from '@/assets/icons/menu.svg'
 
 export default {
     components: {
-        Logo
+        Logo,
+        MenuIcon
     },
     data() {
         return {}
