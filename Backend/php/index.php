bb6c0f3dfe046fb4638b6b0175e02e18beecb4df
Tjark Kühl <tjark-kuehl@hotmail.com>
2019-04-26 13:58:03 +0200

    Api addKunde

--- Backend/php/index.php
+++ Backend/php/index.php
@@ -17,6 +17,37 @@ include_once "./classes/GermanDate.class.php";
 /* Local db variable */
 $db = $GLOBALS["db"];
 
+function checkFieldData(stdClass $data, array $requiredFields)
+{
+    if (empty($data)) {
+        return "Fehler es sind keine POST variabeln gesetzt";
+    }
+
+    // Alle Pflichtfelder durchgehen und gucken ob sie gesetzt wurden
+    foreach ($requiredFields as $idx => $rf) {
+        if (isset($data->{$rf}) && !empty($data->{$rf})) {
+            unset($requiredFields[$idx]);
+        }
+    }
+
+    // Wenn Pflichtfelder noch nicht gesetzt wurden dann return error
+    if (sizeof($requiredFields) !== 0) {
+        return "Fehler folgende Variablen wurden nicht gesetzt: " . implode(", ", $requiredFields);
+    }
+
+    return true;
+}
+
+function formatQueryInput(array $arr)
+{
+    $formattedArray = array();
+    foreach ((array)$arr as $key => $val) {
+        $formattedArray[":{$key}"] = $val;
+    }
+    unset($formattedArray[":action"]);
+    return $formattedArray;
+}
+
 /* Early catch api requests */
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $response = new stdClass();
@@ -24,28 +55,30 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $data = json_decode(file_get_contents("php://input"));
 
     switch ($data->action) {
-        /* #region Kunden */
         case 'addKunde':
-            if (empty($data)) {
-                $error = "Fehler es sind keine POST variabeln gesetzt";
+            // Gibt die field data wieder in der entweder ein Fehler oder ein true enthalten ist
+            $fd = checkFieldData($data, ["name", "strasse", "strassennummer", "plz", "ort", "telefon", "email"]);
+
+            // Wenn die field data nicht true ist dann abbrechen und error ausgeben
+            if ($fd !== true) {
+                $error = $fd;
                 break;
             }
 
-            // Alle Pflichtfelder durchgehen und gucken ob sie gesetzt wurden
-            $required_fields = ["name", "strasse", "strassennummer", "plz", "ort", "telefon"];
-            foreach ($required_fields as $rf) {
-                if (isset($data->{$rf}) && !empty($data->{$rf})) {
-                    unset($rf);
-                }
+            // Bereitet den SQL query vor
+            $stmt = $db->prepare("INSERT INTO kunden (`name`, vorname, strasse, strassennummer, plz, ort, telefon, email) VALUES (:name, :vorname, :strasse, :strassennummer, :plz, :ort, :telefon, :email)");
+
+            // Formatiert die POST Daten um im query verwendet zu werden und führt den query aus
+            try {
+                $stmt->execute((array)$data);
+            } catch (Exception $e) {
+                $error = "Fehler bei der Ausführung des querys in addKunde!";
+                break;
             }
 
-            $stmt = $pdo->prepare("INSERT INTO kunden (`name`, vorname, strasse, strassennummer, plz, ort, telefon, email)
-                                   VALUES (:name, :vorname, :strasse, :strassennummer, :plz, :ort, :telefon, :email)");
-
             break;
-        /* #endregion */
         default:
-            $error = "Action is not set!";
+            $error = "POST Aktion wurde nicht gesetzt.";
             break;
     }
 
