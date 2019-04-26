d3be4d803eb24dc2cc6c913502fc9839bd33bd66
Tjark Kühl <tjark-kuehl@hotmail.com>
2019-04-26 14:09:29 +0200

    Dokumentation

--- Backend/php/index.php
+++ Backend/php/index.php
@@ -10,19 +10,26 @@ header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 header_remove("X-Powered-By");
 
-/* Includes */
+/**
+ * Includes
+ */
 include_once "./includes/database.inc.php";
 include_once "./classes/GermanDate.class.php";
 
-/* Local db variable */
+/**
+ * Globale db Variable zu Lokaler Variable 
+ */
 $db = $GLOBALS["db"];
 
+/**
+ * Überprüft den POST payload ob die Daten mit der Angefragten Funktion überinstimmt
+ *
+ * @param stdClass $data
+ * @param array $requiredFields
+ * @return string|bool
+ */
 function checkFieldData(stdClass $data, array $requiredFields)
 {
-    if (empty($data)) {
-        return "Fehler es sind keine POST variabeln gesetzt";
-    }
-
     // Alle Pflichtfelder durchgehen und gucken ob sie gesetzt wurden
     foreach ($requiredFields as $idx => $rf) {
         if (isset($data->{$rf}) && !empty($data->{$rf})) {
@@ -38,6 +45,12 @@ function checkFieldData(stdClass $data, array $requiredFields)
     return true;
 }
 
+/**
+ * Formatiert das gegebene Array in ein Array welches für Prepared Querys verwendet werden kann
+ *
+ * @param array $arr
+ * @return array
+ */
 function formatQueryInput(array $arr)
 {
     $formattedArray = array();
@@ -48,7 +61,9 @@ function formatQueryInput(array $arr)
     return $formattedArray;
 }
 
-/* Early catch api requests */
+/**
+ * Fängt alle POST requests ab und geht die möglichen Aktionen durch
+ */
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $response = new stdClass();
     $error = null;
@@ -56,19 +71,29 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     switch ($data->action) {
         case 'addKunde':
-            // Gibt die field data wieder in der entweder ein Fehler oder ein true enthalten ist
+            /**
+             * Gibt die field data wieder in der entweder ein Fehler oder ein true 
+             * enthalten ist
+             */
             $fd = checkFieldData($data, ["name", "strasse", "strassennummer", "plz", "ort", "telefon", "email"]);
 
-            // Wenn die field data nicht true ist dann abbrechen und error ausgeben
+            /**
+             * Wenn die field data nicht true ist dann abbrechen und error ausgeben
+             */
             if ($fd !== true) {
                 $error = $fd;
                 break;
             }
 
-            // Bereitet den SQL query vor
+            /**
+             * Bereitet den SQL query vor
+             */
             $stmt = $db->prepare("INSERT INTO kunden (`name`, vorname, strasse, strassennummer, plz, ort, telefon, email) VALUES (:name, :vorname, :strasse, :strassennummer, :plz, :ort, :telefon, :email)");
 
-            // Formatiert die POST Daten um im query verwendet zu werden und führt den query aus
+            /**
+             * Formatiert die POST Daten um im query verwendet zu werden und führt den
+             * query aus
+             */
             try {
                 $stmt->execute((array)$data);
             } catch (Exception $e) {
@@ -82,7 +107,10 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
             break;
     }
 
-    /* Check error response */
+    /**
+     * Fängt aufgetretene Fehler ab oder gibt ein success = true zurück
+     * wenn keine Fehler augetreten ist
+     */
     if (is_null($error)) {
         $response->success = true;
     } else {
@@ -90,6 +118,9 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         $response->success = false;
     }
 
+    /**
+     * Verwandelt die Antwort in ein JSON Objekt und gibt sie aus
+     */
     echo json_encode($response);
 }
 
